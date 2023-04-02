d0303ed102e47656d43f734a61bf05a2c81317b7
aveepsit <aveepsitchowdhury@gmail.com>
2023-04-02 12:34:03 +0530

    add prettier formatting

--- app/src/logger/logger.js
+++ app/src/logger/logger.js
@@ -1,12 +1,12 @@
-import log from "loglevel";
+import log from 'loglevel';
 
-import { CLIENT_LOG_LEVEL } from "constants/constants-logger";
+import { CLIENT_LOG_LEVEL } from 'constants/constants-logger';
 
 export const logger = log.setLevel(CLIENT_LOG_LEVEL);
 
 /**
  * Generates a module logger for the given module name and sets the needed log level.
- * 
+ *
  * @param {string} moduleName name of module.
  * @param {string} [loggingLevel] logging level.
  * @returns {import("loglevel").Logger} logging instance.
