74e4425a38c1d2413f7fc7464e1454a5f59e8dc5
aveepsit <aveepsitchowdhury@gmail.com>
2023-04-29 00:27:51 +0530

    added error constants

---
+++ app/src/constants/constants-error.js
@@ -0,0 +1,5 @@
+export const INVALID_PAGE_ERROR = "INVALID_PAGE_ERROR";
+
+export const ERROR_MESSAGE = {
+  INVALID_PAGE_ERROR: "The requested page does not exist or you do not have permissions to access it",
+};
